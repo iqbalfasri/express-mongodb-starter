8b331e56a36904297e93c034c1e0cb7e036cdf06
Iqbal Fasri <iqbalx64@gmail.com>
2019-04-09 10:54:18 +0700

    Create model and Create Controller

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,9 +1,45 @@
-/**
- * User Controller
+const User = require("../models/User.model");
+const code_response = require("../lib/code-response");
+
+/** 
+ * Mengamil semua data
  */
 exports.index = (req, res) => {
-  res.json("Hello user!");
+  User.find().then(user => {
+    res.status(200).send({
+      code: code_response.CODE_SUCCESS,
+      message: "success",
+      data: user
+    })
+  }).catch(err => {
+    res.status(500).send({
+      code: code_response.CODE_SERVER_ERROR,
+      message: "internal server error"
+    })
+  })
 };
-exports.create = (req, res, next) => {
-  res.json("Create data here");
+
+/**
+ * Menambahkan data baru
+ */
+exports.create = (req, res) => {
+  const { nama, email } = req.body;
+  const createUser = new User({
+    nama: nama,
+    email: email
+  });
+
+  // Save document to database
+  createUser.save().then(user => {
+    res.status(201).send({
+      code: code_response.CODE_SUCCESS,
+      message: "success",
+      data: user
+    });
+  }).catch(err => {
+    res.status(500).send({
+      code: code_response.CODE_SERVER_ERROR,
+      message: "internal server error"
+    })
+  })
 };
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,3 +1,18 @@
-/** 
- * User Model
-*/
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Create user schema
+const UserSchema = new Schema({
+  nama: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+// Create user model
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
