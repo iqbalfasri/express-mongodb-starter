59bcdd653ed1b58def7779297900f9e2672e0d7f
Iqbal Fasri <iqbalx64@gmail.com>
2019-04-09 14:27:38 +0700

    RESTFul Controller done

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -43,3 +43,71 @@ exports.create = (req, res) => {
     })
   })
 };
+
+/**
+ * Melihat detail user
+ */
+exports.detail = (req, res) => {
+  const { _id } = req.params;
+  User.findOne({ _id: _id }).then(user => {
+    res.status(200).send({
+      code: code_response.CODE_SUCCESS,
+      message: "success",
+      data: user
+    })
+  }).catch(err => {
+    res.status(500).send({
+      code: code_response.CODE_SERVER_ERROR,
+      message: err.message
+    })
+  })
+}
+
+/**
+ * Mengubah data / document
+ */
+exports.update = (req, res) => {
+  const { _id } = req.params;
+  const { nama, email } = req.body;
+  const updatedData = {};
+  
+  if (nama) {
+    updatedData.nama = nama;
+  }
+
+  if (email) {
+    updatedData.email = email;
+  }
+
+  User.findOneAndUpdate({ _id: _id }, updatedData).then(user => {
+    res.status(201).send({
+      code: code_response.CODE_SUCCESS,
+      message: "success",
+      data: user
+    })
+  }).catch(err => {
+    res.status(500).send({
+      code: code_response.CODE_SERVER_ERROR,
+      message: err.message
+    })
+  })
+
+}
+
+/**
+ * Menghapus data / document
+ */
+exports.delete = (req, res) => {
+  const { _id } = req.params;
+  User.findByIdAndRemove({ _id: _id }).then(() => {
+    res.status(201).send({
+      code: code_response.CODE_SUCCESS,
+      message: "success",
+    })
+  }).catch(err => {
+    res.status(500).send({
+      code: code_response.CODE_SERVER_ERROR,
+      message: err.message
+    })
+  })
+}
--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -9,5 +9,10 @@ const { validations, schemaValidations } = require("../middleware/validation");
 Router.route("/")
   .get(User.index)
   .post(validations(schemaValidations.users), User.create);
+Router.route("/:_id")
+  .get(User.detail)
+  .put(validations(schemaValidations.users), User.update)
+  .patch(validations(schemaValidations.users), User.update)
+  .delete(User.delete);
 
 module.exports = Router;
