a92fcef96ea3671b229d1ad5b5622418813bd5d3
Iqbal Fasri <iqbalx64@gmail.com>
2019-04-10 14:27:54 +0700

    use ES6 syntax functions

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -4,22 +4,20 @@ const code_response = require("../lib/code-response");
 /**
  * Fungsi untuk mem-validasi data
  */
-exports.validations = schema => {
-  return (req, res, next) => {
-    let data = req.body;
-    Joi.validate(data, schema, (err, value) => {
-      if (err) {
-        // send status 422 error response
-        res.status(422).send({
-          code: code_response.CODE_BAD_REQUEST,
-          status: "Invalid request data",
-          message: err.message
-        });
-      } else {
-        next();
-      }
-    });
-  };
+exports.validations = schema => (req, res, next) => {
+  let data = req.body;
+  Joi.validate(data, schema, (err, value) => {
+    if (err) {
+      // send status 422 error response
+      res.status(422).send({
+        code: code_response.CODE_BAD_REQUEST,
+        status: "Invalid request data",
+        message: err.message
+      });
+    } else {
+      next();
+    }
+  });
 };
 
 /**
